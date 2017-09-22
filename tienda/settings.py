5b2c8c6dbcb922e69b3a8ee7faed0b0c7fe5a51b
Mike Marba <miguel_marba@hotmail.com>
2017-09-22 00:02:04 -0500

    Agrego el path para staticfiles

--- tienda/settings.py
+++ tienda/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 LOGIN_URL = '/inmobiliaria/login/'
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
